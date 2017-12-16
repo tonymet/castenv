2b4dac6a5c845bb65f0aaa35560ee64b866e3488
Anthony Metzidis <anthony.metzidis@gmail.com>
2017-12-16 10:09:40 -0800

    initial typescript test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+declare var process:  { [env: string]: {[key:string]: ConvertedEnvironmentValue }  }  
+type ConvertedEnvironmentValue = string|number|boolean|undefined
 /**
  * @param {*} v cast using JSON.parse 
  */
-function castValue(v){
+function castValue(v): string|number{
     try{
         return JSON.parse(v)
     }
@@ -12,8 +14,8 @@ function castValue(v){
 /**
  * cast process.env and return
  */
-function env(){
-    var converted = {}
+function env(): {string?: ConvertedEnvironmentValue}{
+    let converted: {string?: ConvertedEnvironmentValue} = {}
     Object.keys(process.env).forEach(k => {
         converted[k] = castValue(process.env[k])
     })
@@ -22,7 +24,7 @@ function env(){
 /**
  * overwrite process.env
  */
-function cast(){
+function cast(): void{
     process.env = env()
 }
 module.exports = {castValue, env, cast}
